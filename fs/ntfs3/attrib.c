d5e82d6d00eef6aaa5177d58aea58f693c1c47fe
Kari Argillander <kari.argillander@gmail.com>
2021-09-07 17:28:42 +0300

    fs/ntfs3: Use min/max macros instated of ternary operators
    We can make code little bit more readable by using min/max macros.
    
    These were found with Coccinelle.
    
    Signed-off-by: Kari Argillander <kari.argillander@gmail.com>
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>

--- fs/ntfs3/attrib.c
+++ fs/ntfs3/attrib.c
@@ -13,6 +13,7 @@
 #include <linux/nls.h>
 #include <linux/ratelimit.h>
 #include <linux/slab.h>
+#include <linux/kernel.h>
 
 #include "debug.h"
 #include "ntfs.h"
@@ -1966,7 +1967,7 @@ int attr_punch_hole(struct ntfs_inode *ni, u64 vbo, u64 bytes, u32 *frame_size)
 			return 0;
 
 		from = vbo;
-		to = (vbo + bytes) < data_size ? (vbo + bytes) : data_size;
+		to = min_t(u64, vbo + bytes, data_size);
 		memset(Add2Ptr(resident_data(attr_b), from), 0, to - from);
 		return 0;
 	}
