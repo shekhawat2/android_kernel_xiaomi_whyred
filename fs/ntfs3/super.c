fde46675bac7a4980dd36aee1d1610b0fe98bd0c
Kari Argillander <kari.argillander@gmail.com>
2021-09-09 21:09:38 +0300

    fs/ntfs3: Remove tmp var is_ro in ntfs_fill_super
    We only use this in two places so we do not really need it. Also
    wrapper sb_rdonly() is pretty self explanatory. This will make little
    bit easier to read this super long variable list in the beginning of
    ntfs_fill_super().
    
    Signed-off-by: Kari Argillander <kari.argillander@gmail.com>
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>

--- fs/ntfs3/super.c
+++ fs/ntfs3/super.c
@@ -922,7 +922,6 @@ static int ntfs_fill_super(struct super_block *sb, void *data, int silent)
 	struct ATTR_DEF_ENTRY *t;
 	u16 *upcase = NULL;
 	u16 *shared;
-	bool is_ro;
 	struct MFT_REF ref;
 
 	ref.high = 0;
@@ -1062,16 +1061,14 @@ static int ntfs_fill_super(struct super_block *sb, void *data, int silent)
 
 	iput(inode);
 
-	is_ro = sb_rdonly(sb);
-
 	if (sbi->flags & NTFS_FLAGS_NEED_REPLAY) {
-		if (!is_ro) {
+		if (!sb_rdonly(sb)) {
 			ntfs_warn(sb,
 				  "failed to replay log file. Can't mount rw!");
 			return -EINVAL;
 		}
 	} else if (sbi->volume.flags & VOLUME_FLAG_DIRTY) {
-		if (!is_ro && !sbi->options->force) {
+		if (!sb_rdonly(sb) && !sbi->options->force) {
 			ntfs_warn(
 				sb,
 				"volume is dirty and \"force\" flag is not set!");
