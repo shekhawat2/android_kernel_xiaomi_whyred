cac8fbb5cd644b696ee69b9263b90865ec07231a
Kunmun <kunmun.devroms@gmail.com>
2022-02-01 12:42:34 +0530

    ion_legacy: Move few of the userspace APIs to `uapi/ion.h`
    • OSS Camera HAL depends on these APIs and for some reason it's not including ion_legacy.h
      Hence, move these APIs into the non-legacy ion userspace API header

--- drivers/staging/android/ion/ion_legacy.h
+++ drivers/staging/android/ion/ion_legacy.h
@@ -5,60 +5,6 @@
 
 #include "../uapi/ion.h"
 
-typedef int ion_user_handle_t;
-
-/**
- * struct ion_old_allocation_data - metadata passed from legacy (pre-4.12 kernel)
- * 				       userspace for allocations
- * @len:		size of the allocation
- * @align:		required alignment of the allocation
- * @heap_id_mask:	mask of heap ids to allocate from
- * @flags:		flags passed to heap
- * @handle:		pointer that will be populated with a cookie to use to
- *			refer to this allocation
- *
- * Provided by userspace as an argument to the ioctl
- */
-struct ion_old_allocation_data {
-	size_t len;
-	size_t align;
-	unsigned int heap_id_mask;
-	unsigned int flags;
-	ion_user_handle_t handle;
-};
-
-/**
- * struct ion_fd_data - metadata passed to/from userspace for a handle/fd pair
- * @handle:	a handle
- * @fd:		a file descriptor representing that handle
- *
- * For ION_IOC_SHARE or ION_IOC_MAP userspace populates the handle field with
- * the handle returned from ion alloc, and the kernel returns the file
- * descriptor to share or map in the fd field.  For ION_IOC_IMPORT, userspace
- * provides the file descriptor and the kernel returns the handle.
- */
-struct ion_fd_data {
-	ion_user_handle_t handle;
-	int fd;
-};
-
-/**
- * struct ion_handle_data - a handle passed to/from the kernel
- * @handle:	a handle
- */
-struct ion_handle_data {
-	ion_user_handle_t handle;
-};
-
-/**
- * DOC: ION_OLD_IOC_ALLOC - allocate memory (pre-4.12 version)
- *
- * Takes an ion_old_allocation_data struct and returns it with the handle field
- * populated with the opaque handle for the allocation.
- */
-#define ION_OLD_IOC_ALLOC	_IOWR(ION_IOC_MAGIC, 0, \
-				      struct ion_old_allocation_data)
-
 /**
  * DOC: ION_IOC_FREE - free memory
  *
