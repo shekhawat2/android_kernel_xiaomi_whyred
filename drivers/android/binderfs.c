dd461a750a873dece56745135d5a5c49311ced08
Anand Shekhawat <anandsingh215@yahoo.com>
2022-09-24 08:40:38 +0530

    drivers: android: Revert "fs: make helpers idmap mount aware"
    This reverts commit https://android.googlesource.com/kernel/common/+/549c7297717c32ee53f156cd949e055e601f67bb

--- drivers/android/binderfs.c
+++ drivers/android/binderfs.c
@@ -372,8 +372,7 @@ static inline bool is_binderfs_control_device(const struct dentry *dentry)
 	return info->control_dentry == dentry;
 }
 
-static int binderfs_rename(struct user_namespace *mnt_userns,
-			   struct inode *old_dir, struct dentry *old_dentry,
+static int binderfs_rename(struct inode *old_dir, struct dentry *old_dentry,
 			   struct inode *new_dir, struct dentry *new_dentry,
 			   unsigned int flags)
 {
@@ -381,8 +380,7 @@ static int binderfs_rename(struct user_namespace *mnt_userns,
 	    is_binderfs_control_device(new_dentry))
 		return -EPERM;
 
-	return simple_rename(&init_user_ns, old_dir, old_dentry, new_dir,
-			     new_dentry, flags);
+	return simple_rename(old_dir, old_dentry, new_dir, new_dentry, flags);
 }
 
 static int binderfs_unlink(struct inode *dir, struct dentry *dentry)
